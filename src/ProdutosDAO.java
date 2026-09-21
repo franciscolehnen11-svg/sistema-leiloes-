0716c8124611a56b13520a13b292d73dd65a1504
franciscolehnen11-svg <franciscolehnen11@gmail.com>
2026-09-21 17:03:22 -0300

    Listagem de produtos cadastrados funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -50,6 +50,32 @@ public class ProdutosDAO {
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
+        String sql = "SELECT id, nome, valor, status FROM produtos";
+        
+        listagem.clear();
+        conn = new conectaDAO().connectDB();
+        if (conn == null) {
+            return listagem;
+        }
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException erro) {
+            System.out.println("Erro ao listar produtos: " + erro.getMessage());
+        } finally {
+            fecharRecursos();
+        }
+        
         return listagem;
     }
     
